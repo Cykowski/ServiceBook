28329d2dff8e8856f63972630a33d3d739a22dcb
Cykowski <adamszet@gmail.com>
2017-02-23 17:05:14 +0100

    dodano test (tak na probe)
    Signed-off-by: Cykowski <adamszet@gmail.com>

---
+++ service_book/tests/service_book/vehicle/TestPart.java
@@ -0,0 +1,29 @@
+package service_book.vehicle;
+
+import static org.junit.Assert.*;
+
+import java.math.BigDecimal;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import service_book.vehicle.Part;
+
+public class TestPart {
+
+	@org.junit.Test
+	public void testSetServicePrice() {
+		Part part = new Part();
+		part.setServicePrice(new BigDecimal("0.123"));
+		Assert.assertEquals("0.12" ,part.getServicePrice().toString());
+//		fail("Not yet implemented");
+
+		part = new Part();
+		part.setServicePrice(new BigDecimal("0.128"));
+		Assert.assertEquals("0.13" ,part.getServicePrice().toString());
+
+		part = new Part();
+		part.setServicePrice(new BigDecimal("0.1"));
+		Assert.assertEquals("0.10" ,part.getServicePrice().toString());
+	}
+}
